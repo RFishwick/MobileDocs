f8cce3a923462fd0499ed58e11a96a8314466bf3
Randy Fishwick <rfishwick@hotmail.com>
2018-02-01 20:28:57 -0800

    Add list_comprehension

---
+++ list_comprehension.py
@@ -0,0 +1,15 @@
+my_list = [0, 1, 2, 3, 4]
+print('my_list:\t\t', my_list)
+
+an_equal_list = [x for x in range(5)]
+print('an_equal_list:\t', an_equal_list)
+
+multiply_list = [x * 3 for x in range(5)]
+print('multiply_list:\t', multiply_list)
+
+print(8 % 3)  # 8 / 3 == 2R2, so 8 % 3 == 2 (modulus operator)
+print(8 / 3)
+
+print(9 % 3)
+print(10 % 3)
+print(10 % 2)
